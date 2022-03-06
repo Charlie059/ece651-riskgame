415baab325df6a8299ffb1caa99c3ef4d5ee81a9
xg73 <xg73@duke.edu>
2022-03-06 16:29:49 -0500

    Update Server.java

--- server/src/main/java/edu/duke/ece651/server/Server.java
+++ server/src/main/java/edu/duke/ece651/server/Server.java
@@ -17,7 +17,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Server {
-
+  // ServerJSON
   public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
     if (args.length != 1) {
       throw new IllegalArgumentException("Syntex: ./Server <port>");
@@ -27,7 +27,7 @@ public class Server {
     ServerSocket serversocket = new ServerSocket(Integer.parseInt(args[0]));
 
     // The max number of throwed threads by service
-    ExecutorService service = Executors.newFixedThreadPool(2);
+    ExecutorService service = Executors.newFixedThreadPool(16);
     ArrayList<Future<?>> futureList = new ArrayList<>();
 
     // Connect HostServer, ask num_player
@@ -36,11 +36,16 @@ public class Server {
     clientSocketList.add(hostsocket);
     InputStream in = hostsocket.getInputStream();
     OutputStream out = hostsocket.getOutputStream();
-    var hostwriter = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+
+//    var hostwriter = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+    var hostwriter = new OutputStreamWriter(out, StandardCharsets.UTF_8);
     var hostreader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 
+
     //Send special char to HostClient
-    hostwriter.write("H");
+    hostwriter.write("H" + "\n");
+    hostwriter.flush();
+
 
     //Get num_player
     String s = hostreader.readLine();
@@ -57,7 +62,8 @@ public class Server {
       Socket clientsocket = serversocket.accept();
       OutputStream client_out = clientsocket.getOutputStream();
       var ClientWriter = new BufferedWriter(new OutputStreamWriter(client_out,StandardCharsets.UTF_8));
-      ClientWriter.write("C");
+      ClientWriter.write("C\n");
+      ClientWriter.flush();
       clientSocketList.add(clientsocket);
     }
 
