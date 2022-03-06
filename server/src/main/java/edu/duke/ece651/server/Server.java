d31e6d5811140bd8fef220df61bbbaf557b46658
Yuxuan Yang <yuxuan@Yuxuans-MacBook-Pro.local>
2022-03-05 23:45:36 -0500

    update issue 3

--- server/src/main/java/edu/duke/ece651/server/Server.java
+++ server/src/main/java/edu/duke/ece651/server/Server.java
@@ -19,37 +19,37 @@ public class Server {
     if (args.length != 1) {
       throw new IllegalArgumentException("Syntex: ./Server <port>");
     }
-    
+
     System.out.println("On port" + args[0]);
     ServerSocket serversocket = new ServerSocket(Integer.parseInt(args[0]));
 
-    //The max number of throwed threads by service
+    // The max number of throwed threads by service
     ExecutorService service = Executors.newFixedThreadPool(2);
     ArrayList<Future<?>> futureList = new ArrayList<>();
 
-    //add num_player
+    // add num_player
     ArrayList<Socket> clientSocketList = new ArrayList<Socket>();
     Socket hostsocket = serversocket.accept();
     clientSocketList.add(hostsocket);
     InputStream in = hostsocket.getInputStream();
     var hostreader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
     String s = hostreader.readLine();
-    Integer num_player = Integer.getInteger(s);
+    Integer num_player = Integer.parseInt(s);
 
-    for(int i = 0; i<num_player-1;i++){
+    for (int i = 0; i < num_player - 1; i++) {
       Socket clientsocket = serversocket.accept();
       clientSocketList.add(clientsocket);
     }
-    
-    for(int k=0;k<num_player;k++) {
+
+    for (int k = 0; k < num_player; k++) {
       try {
         ServerCallable task = new ServerCallable(clientSocketList.get(k));
         Future<?> future = service.submit(task);
         futureList.add(future);
       } finally {
-        
-        }
+
       }
     }
-  
+  }
+
 }
