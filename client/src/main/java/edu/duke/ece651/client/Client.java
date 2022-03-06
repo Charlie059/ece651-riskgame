ad2b4431453897c5517f44c79fe957ac27af1f51
xg73 <xg73@duke.edu>
2022-03-06 16:32:23 -0500

    Update Client.java

--- client/src/main/java/edu/duke/ece651/client/Client.java
+++ client/src/main/java/edu/duke/ece651/client/Client.java
@@ -1,7 +1,6 @@
 package edu.duke.ece651.client;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,18 +13,21 @@ import java.nio.charset.StandardCharsets;
 public class Client {
 
   public static void main(String[] args) throws IOException {
-    Socket socket = new Socket("localhost", 12345);
-    OutputStream out = socket.getOutputStream();
+    Socket socket = new Socket("127.0.0.1", 4444);
+    System.out.println("Connection Success");
     InputStream in = socket.getInputStream();
+    OutputStream out = socket.getOutputStream();
+    var writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
     var reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-    var writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
 
     String s = reader.readLine();
+    System.out.println(s);
+
     if(s.equals("H")){
       System.out.println("You are Host!, please send number of players: ");
-      int total_player = 3;
+      int total_player = 2;
       System.out.println("You entered " + String.valueOf(total_player));
-      writer.write(String.valueOf(total_player));
+      writer.write(String.valueOf(total_player)+"\n");
       writer.flush();
     }else{
       System.out.println("You are Player");
@@ -33,11 +35,16 @@ public class Client {
 
     while(true){
       //receive()
-    String received_message = reader.readLine();
-    //parse()
-    System.out.println(received_message);
-    //confirm()
-    writer.write("I Know!");
+      String received_message = reader.readLine();
+      System.out.println("Client received message:");
+
+      //parse()
+      System.out.println(received_message);
+
+      //confirm()
+      writer.write("I Know!" + "\n");
+      writer.flush();
+
     }
   }
 }
