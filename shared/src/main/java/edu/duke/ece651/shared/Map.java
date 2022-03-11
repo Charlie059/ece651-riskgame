3f1215255420197839261d3756b443f94231a08e
Junfeng Zhi <jz399@duke.edu>
2022-03-10 23:19:31 -0500

    create prototype functions

--- shared/src/main/java/edu/duke/ece651/shared/Map.java
+++ shared/src/main/java/edu/duke/ece651/shared/Map.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Map {
-    public int numOfPlayers;//number of player = 3
+    public int numOfPlayers; // number of player = 3
     MapFactory myMapFactory;
     HashMap<String, Territory> territoryList;
-    ArrayList<ArrayList<String>> groups; 
+    ArrayList<ArrayList<String>> groups;
+
     /**
      * constructor
+     * 
      * @param _numOfPlayers
      */
     public Map(int _numOfPlayers) {
@@ -20,17 +22,18 @@ public class Map {
     }
 
     /**
-     * Use to test whether each territory has correct neighbour and whether groups is correct
+     * Use to test whether each territory has correct neighbour and whether groups
+     * is correct
      */
     void showNeighbours_TEST() {
         for (String name : territoryList.keySet()) {
             System.out.print("territory[" + name + "]: ");
-            for (Territory t:territoryList.get(name).neighbours) {
-                System.out.print(t.getName()+" ");
+            for (Territory t : territoryList.get(name).neighbours) {
+                System.out.print(t.getName() + " ");
             }
             System.out.print("\n");
         }
-        for(ArrayList<String> group:groups){
+        for (ArrayList<String> group : groups) {
             for (String name : group) {
                 System.out.print(name + " ");
             }
@@ -45,5 +48,30 @@ public class Map {
     public ArrayList<ArrayList<String>> getGroups() {
         return groups;
     }
-}
 
+    /**
+     * Used in Move action, check if there is a path between the two
+     * territories(with names "from" and "to") of the player (playerID).
+     * 
+     * @param playerID
+     * @param from
+     * @param to
+     * @return return true if path existed, else return false.
+     */
+    public boolean isPathExist(int playerID, String from, String to) {
+        return true;
+    }
+
+    /**
+     * Used in Attack action, check if the two territories (with names "Terr1" and
+     * "Terr2") are neighbors and belong to different players. ? 
+     * 
+     * @param playerID
+     * @param Terr1
+     * @param Terr2
+     * @return
+     */
+    public boolean isAdjacent(int playerID, String Terr1, String Terr2) {
+        return true;
+    }
+}
