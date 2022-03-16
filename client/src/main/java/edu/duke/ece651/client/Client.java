c4dcba3733aaf37f5e1006c2d79945f722be8959
Yuxuan Yang <yuxuan@Yuxuans-MacBook-Pro.local>
2022-03-16 17:09:14 -0400

    host number of player Client checker

--- client/src/main/java/edu/duke/ece651/client/Client.java
+++ client/src/main/java/edu/duke/ece651/client/Client.java
@@ -3,7 +3,6 @@
  */
 package edu.duke.ece651.client;
 
-
 import edu.duke.ece651.shared.ClientJSON;
 
 import java.io.*;
@@ -11,7 +10,6 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit; // TODO DELETE
-
 
 public class Client {
 
@@ -24,9 +22,9 @@ public class Client {
   private final BufferedReader reader;
   private final Socket socket;
 
-
   /**
    * Constructor of Client Class by given portNum and host
+   * 
    * @param portNum
    * @param host
    * @throws IOException
@@ -43,16 +41,18 @@ public class Client {
 
   /**
    * Send a JSON to the server, and it adds a \n automatically
+   * 
    * @param msg to be send
    * @throws IOException
    */
   public void sendMsg(String msg) throws IOException {
-      this.writer.write(msg + "\n");
-      this.writer.flush();
+    this.writer.write(msg + "\n");
+    this.writer.flush();
   }
 
   /**
    * Receive messgae from the server
+   * 
    * @return String
    * @throws IOException
    */
@@ -60,32 +60,45 @@ public class Client {
     return this.reader.readLine();
   }
 
-
   /**
-   * Client init player. If cilent is first to connect the server then we define this client is host.
-   * Host should input the total number of player and then server will be based on that value to
-   * let the client join in.
+   * Client init player. If cilent is first to connect the server then we define
+   * this client is host. Host should input the total number of player and then
+   * server will be based on that value to let the client join in.
+   * 
    * @throws IOException
    */
   public void initPlayer() throws IOException {
     String msg = this.recvMsg();
-    if(msg.equals("1")){ // If msg indicate that player is the host
+    if (msg.equals("1")) { // If msg indicate that player is the host
       System.out.println("You are Host!, please send number of players: ");
 
-      //TODO user input checker class
+      // TODO user input checker class
       Scanner reader = new Scanner(System.in);
-      System.out.println("Enter a number: ");
-      int total_player = reader.nextInt();
-      System.out.println("You entered " + String.valueOf(total_player));
+
+      ClientRuleChecker checkerHostNumPlayer = new CheckerHostNumPlayer();
+      String userInput;
+      String checkerOutput;
+      do {
+        System.out.println("Enter a number: ");
+        userInput = reader.nextLine();
+        checkerOutput = checkerHostNumPlayer.doCheck(userInput);
+        System.out.println("You entered " + String.valueOf(userInput));
+        System.out.println(checkerOutput);
+      } while (!checkerOutput.isEmpty());
+      
+      int total_player = Integer.parseInt(userInput);
+
       this.sendMsg(String.valueOf(total_player));
 
       // Create a new player object for client
-      this.player = new Player(Integer.parseInt(msg), total_player, new BufferedReader(new InputStreamReader(System.in)), System.out);
-    }else{ // If msg indicate that player is non-host
+      this.player = new Player(Integer.parseInt(msg), total_player,
+          new BufferedReader(new InputStreamReader(System.in)), System.out);
+    } else { // If msg indicate that player is non-host
       String[] parsedMsg = msg.split("\\s+");
 
       // Create a new player object for client
-      this.player = new Player(Integer.parseInt(parsedMsg[0]), Integer.parseInt(parsedMsg[1]), new BufferedReader(new InputStreamReader(System.in)), System.out);
+      this.player = new Player(Integer.parseInt(parsedMsg[0]), Integer.parseInt(parsedMsg[1]),
+          new BufferedReader(new InputStreamReader(System.in)), System.out);
     }
 
   }
@@ -95,8 +108,7 @@ public class Client {
     Client client = null;
     try {
       client = new Client(1651, "127.0.0.1");
-    }
-    catch (IOException ioException){
+    } catch (IOException ioException) {
       System.out.println("Cannot connect to the server");
       int EXIT_FAILURE = 1;
       System.exit(EXIT_FAILURE);
@@ -105,7 +117,7 @@ public class Client {
     // First handshake, Client recv msg from server and create player object
     client.initPlayer();
 
-    while(true){
+    while (true) {
       // Client recv msg
       String received_message = client.recvMsg();
 
@@ -114,7 +126,7 @@ public class Client {
 
       // Client play one round: let the user do some actions
       client.player.playOneRound();
-      ClientJSON converter = new ClientJSON(client.player.getId(),client.player.getActionList());
+      ClientJSON converter = new ClientJSON(client.player.getId(), client.player.getActionList());
 
       // Client send the actionJSON(ie. ClientJSON) to the server
       client.sendMsg(String.valueOf(converter.convertTo()));
