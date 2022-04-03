68066e3cbacb58580496855d411e95e4e07c3b23
Yuxuan Yang <yy340@duke.edu>
2022-04-03 12:58:59 -0400

    Delete Possible temp files

--- server/build/reports/clover/html/treemap-dash-json.js
+++
@@ -1,7 +0,0 @@
-var treeMapJson = {"id":"Clover database Sun Feb 27 2022 04:22:50 EST0","name":"","data":{
-    "$area":7.0,"$color":71.42857,"title":" 7 Elements, 71.4% Coverage"},
-  "children":[{"id":"edu.duke.ece651.server0","name":
-      "edu.duke.ece651.server","data":{"$area":7.0,"$color":71.42857,"title":
-        "edu.duke.ece651.server 7 Elements, 71.4% Coverage"},"children":[]}]}
-;
-processTreeMapDashJson (treeMapJson);
