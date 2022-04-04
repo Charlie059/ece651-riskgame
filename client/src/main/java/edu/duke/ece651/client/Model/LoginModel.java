249ef3c23f3bec7870c7bb143fa4afcf2f98d31c
XG <41360561+Charlie059@users.noreply.github.com>
2022-04-04 15:22:16 -0400

    Add debug mode for model

--- client/src/main/java/edu/duke/ece651/client/Model/LoginModel.java
+++ client/src/main/java/edu/duke/ece651/client/Model/LoginModel.java
@@ -7,9 +7,40 @@ import edu.duke.ece651.shared.IO.ServerResponse.Response;
 import edu.duke.ece651.shared.Wrapper.AccountID;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class LoginModel extends Model{
+
+    /**
+     * Debug mode
+     * @param userName
+     * @param passWord
+     * @param debugMode
+     * @return
+     */
+    public boolean validateLogin(String userName, String passWord, Boolean debugMode){
+        // If in debug mode return true
+        if(debugMode) return true;
+
+        // Create a new LoginAction
+        LoginAction loginAction = new LoginAction(new AccountID(userName),passWord);
+        // Send to Server to validate
+        try {
+            ClientSocket clientSocket =  ClientSocket.getInstance();
+            clientSocket.sendObject(loginAction);
+            Response response = (Response) ClientSocket.getInstance().recvObject();
+            return response.getClass() == RSPLoginSuccess.class;
+        } catch (IOException | ClassNotFoundException | ClassCastException e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * Validate
+     * @param userName
+     * @param passWord
+     * @return
+     */
     public boolean validateLogin(String userName, String passWord){
         // Create a new LoginAction
         LoginAction loginAction = new LoginAction(new AccountID(userName),passWord);
