a8b0501b370fb3386c67007a8fc6ac27e3cd784b
Zwj321123 <2484617356@qq.com>
2022-04-05 00:35:38 -0400

    add map view for player 3,4,5

---
+++ client/src/main/java/edu/duke/ece651/client/View/MapForPlayer3View.java
@@ -0,0 +1,17 @@
+package edu.duke.ece651.client.View;
+
+import edu.duke.ece651.client.Model.Model;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MapForPlayer3View implements View {
+    @Override
+    public void show(Stage window, Model model) throws IOException {
+        // load start view fxml
+        URL xmlResource = getClass().getResource("/xml/mapForPlayer3.fxml");
+        FXMLLoader loader = new FXMLLoader(xmlResource);
+    }
+}
