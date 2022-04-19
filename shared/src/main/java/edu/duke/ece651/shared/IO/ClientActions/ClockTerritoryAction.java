08cbf511c0af73fcaad78aa945fd3fd415595347
XG <41360561+Charlie059@users.noreply.github.com>
2022-04-19 19:23:26 -0400

    Add Clocking Action AND RSP

---
+++ shared/src/main/java/edu/duke/ece651/shared/IO/ClientActions/ClockTerritoryAction.java
@@ -0,0 +1,10 @@
+package edu.duke.ece651.shared.IO.ClientActions;
+
+import edu.duke.ece651.shared.Visitor.ActionVisitor;
+
+public class ClockTerritoryAction implements Action{
+    @Override
+    public void accept(ActionVisitor actionVisitor) {
+
+    }
+}
