b2390b80fb8098c3cc30e3bc6fb32ecae51e5da3
JFFF <59811560+JunFenngZhi@users.noreply.github.com>
2022-04-20 15:42:29 -0400

    finish cloak territory and deploySpy

---
+++ client/src/main/java/edu/duke/ece651/client/View/CloakDialogView.java
@@ -0,0 +1,38 @@
+package edu.duke.ece651.client.View;
+
+
+import edu.duke.ece651.client.Controller.CloakDialogController;
+import edu.duke.ece651.client.Model.Model;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+
+public class CloakDialogView implements View {
+
+    @Override
+    public void show(Stage window, Model model, boolean debug) throws IOException {
+        // load start view fxml
+        URL xmlResource = getClass().getResource("/xml/cloakDialogView.fxml");
+        FXMLLoader loader = new FXMLLoader(xmlResource);
+
+        HashMap<Class<?>,Object> controllers = new HashMap<>();
+        controllers.put(CloakDialogController.class, new CloakDialogController(window,debug));
+        loader.setControllerFactory(controllers::get);
+        Pane pane = loader.load();
+
+        // create scene and load css
+        Scene scene = new Scene(pane);
+        URL cssResource = getClass().getResource("/css/button.css");
+        scene.getStylesheets().add(cssResource.toString());
+
+        window.setScene(scene);
+        window.setTitle("Cloak");
+        window.showAndWait();
+    }
+}
