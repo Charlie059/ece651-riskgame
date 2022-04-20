718622f3697e15a5c79bdbc486b275b70a1b8c0e
Yuxuan Yang <yy340@duke.edu>
2022-04-20 15:15:51 -0400

    Update New CardType with Cost Involve

--- shared/src/main/java/edu/duke/ece651/shared/Wrapper/CardType.java
+++ shared/src/main/java/edu/duke/ece651/shared/Wrapper/CardType.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardType {
-    private final Integer specialSpyUpgradeCost = 1;
-    private final Integer unitDeployCost = 1;
-    private final List<Integer> specialSpyUpgrade =  List.of(11,specialSpyUpgradeCost);
-    private final List<Integer> unitDeploy = List.of(12,unitDeployCost);
+    private final List<Integer> specialSpyUpgrade =  List.of(11,20);
+    private final List<Integer> unitDeploy = List.of(12,20);
     public List<Integer> SpecialSpyUpgrade(){
         return specialSpyUpgrade;
     }
