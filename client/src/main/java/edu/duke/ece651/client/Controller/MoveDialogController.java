0786e71f11fbd05eddafbc6968a828c214d2321e
XG <41360561+Charlie059@users.noreply.github.com>
2022-04-21 18:43:55 -0400

    Update model deploy spy

--- client/src/main/java/edu/duke/ece651/client/Controller/MoveDialogController.java
+++ client/src/main/java/edu/duke/ece651/client/Controller/MoveDialogController.java
@@ -3,6 +3,7 @@ package edu.duke.ece651.client.Controller;
 
 import edu.duke.ece651.client.Model.GameModel;
 import edu.duke.ece651.client.View.MapView;
+import edu.duke.ece651.shared.map.Spy;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,10 +17,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 public class MoveDialogController implements Initializable,Communication {
@@ -107,7 +105,14 @@ public class MoveDialogController implements Initializable,Communication {
             lv4_n.setText(String.valueOf(unitNumList.get(4)));
             lv5_n.setText(String.valueOf(unitNumList.get(5)));
             lv6_n.setText(String.valueOf(unitNumList.get(6)));
-            spy_n.setText("NA");
+            // Setup spy
+            HashMap<String, ArrayList<Spy>> spyInfo = GameModel.getInstance().getClientPlayerPacket().getSpyInfo();
+            if(spyInfo.containsKey(clickTerr)){
+                spy_n.setText(String.valueOf(spyInfo.get(clickTerr).size()));
+            }
+            else{
+                spy_n.setText("0");
+            }
 
 
             // Get all myTerr
@@ -140,7 +145,14 @@ public class MoveDialogController implements Initializable,Communication {
             lv4_n.setText(String.valueOf(unitNumList.get(4)));
             lv5_n.setText(String.valueOf(unitNumList.get(5)));
             lv6_n.setText(String.valueOf(unitNumList.get(6)));
-            spy_n.setText("NA");
+            // Setup spy
+            HashMap<String, ArrayList<Spy>> spyInfo = GameModel.getInstance().getClientPlayerPacket().getSpyInfo();
+            if(spyInfo.containsKey(clickTerr)){
+                spy_n.setText(String.valueOf(spyInfo.get(clickTerr).size()));
+            }
+            else{
+                spy_n.setText("0");
+            }
 
             // Add levelList
             levelList.add(0);
