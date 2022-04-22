a435be6197abb83a99eab4033aecb9a5c161c360
Zwj321123 <2484617356@qq.com>
2022-04-21 23:40:31 -0400

    change cost of daybreaks

--- shared/src/main/java/edu/duke/ece651/shared/Wrapper/CardType.java
+++ shared/src/main/java/edu/duke/ece651/shared/Wrapper/CardType.java
@@ -7,7 +7,7 @@ public class CardType {
     private final List<Integer> bombardment =  List.of(11,120); //(card_type_index, cost(points))
     private final List<Integer> sanction =  List.of(12,300);
     private final List<Integer> greatLeapForward =  List.of(13,300);
-    private final List<Integer> dayBreaks =  List.of(14,200);
+    private final List<Integer> dayBreaks =  List.of(14,600);
     private final List<Integer> GodBeWithYou =  List.of(15,150);
     private final List<Integer> specialSpyUpgrade =  List.of(16,120);
     private final List<Integer> unitDeploy = List.of(17,0);
